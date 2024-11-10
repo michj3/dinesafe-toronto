01eb895f107eca382dfc6ead3aae5ecd1ec8cb02
michj3 <michelle.ji@mail.utoronto.ca>
2024-11-10 12:57:51 -0500

    commit cleaned data script

--- scripts/03-clean_data.R
+++ scripts/03-clean_data.R
@@ -1,44 +1,33 @@
 #### Preamble ####
-# Purpose: Cleans the raw plane data recorded by two observers..... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 6 April 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
-# License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Purpose: Cleans raw dinesafe data
+# Author: Michelle Ji
+# Date: 10 November 2024
+# Contact: michelle.ji@mail.utoronto.ca
 
-#### Workspace setup ####
+#### Workplace setup ####
 library(tidyverse)
 
-#### Clean data ####
-raw_data <- read_csv("inputs/data/plane_data.csv")
+#### Data cleaning ####
+# read raw data file in 
+raw_dinesafe_data <- read_csv("data/01-raw_data")
 
-cleaned_data <-
-  raw_data |>
-  janitor::clean_names() |>
-  select(wing_width_mm, wing_length_mm, flying_time_sec_first_timer) |>
-  filter(wing_width_mm != "caw") |>
-  mutate(
-    flying_time_sec_first_timer = if_else(flying_time_sec_first_timer == "1,35",
-                                   "1.35",
-                                   flying_time_sec_first_timer)
-  ) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "490",
-                                 "49",
-                                 wing_width_mm)) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "6",
-                                 "60",
-                                 wing_width_mm)) |>
-  mutate(
-    wing_width_mm = as.numeric(wing_width_mm),
-    wing_length_mm = as.numeric(wing_length_mm),
-    flying_time_sec_first_timer = as.numeric(flying_time_sec_first_timer)
-  ) |>
-  rename(flying_time = flying_time_sec_first_timer,
-         width = wing_width_mm,
-         length = wing_length_mm
-         ) |> 
-  tidyr::drop_na()
+# pull out variables of interest
+dinesafe_data <- raw_dinesafe_data |>
+  select(`Establishment Type`, `Min. Inspections Per Year`, `Severity`, `Action`)
 
-#### Save data ####
-write_csv(cleaned_data, "outputs/data/analysis_data.csv")
+# rename variables
+dinesafe_data <- dinesafe_data |>
+  rename(establishment_type = `Establishment Type`,
+         min_inspections = `Min. Inspections Per Year`,
+         severity = `Severity`,
+         action = `Action`)
+
+# get rid of na rows
+dinesafe_data <- dinesafe_data |>
+  drop_na()
+
+dinesafe_data <- dinesafe_data |>
+  filter(severity != "NA - Not Applicable")
+
+#### Save as cleaned data ####
+write_csv(dinesafe_data, "data/02-analysis_data")
